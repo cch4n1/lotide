f6711f95296962e5ba3d35e49d960e7f30fc236f
Charlie C <zbyabqrc@pm.me>
2023-03-25 21:33:38 +0000

    Changed string concatenation to template literals.

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
   actual === expected ?
-    console.log("✅ Assertion Passed: " + actual + " === " + expected)
-    : console.log("🚫 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
