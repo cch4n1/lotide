8ff04e2ca0ad386b10a21b8adc6aa5058cc28fd9
Charlie C <zbyabqrc@pm.me>
2023-03-25 06:07:47 +0000

    Created assertEqual function to test code.

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log("✅ Assertion Passed: " + actual + " === " + expected)
+    : console.log("🚫 Assertion Failed: " + actual + " !== " + expected);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Lighthouse", "Lighthouse");
