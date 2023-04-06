21ecb2f4bed8ca345db78224e3db2dc2a1d8288e
Charlie C <zbyabqrc@pm.me>
2023-04-06 03:36:25 +0000

    created findKeyByValue function to compare a string value with an object and return the first key which contains the value.

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//findKeyByValue starts here
+
+const findKeyByValue = function(object, value) {
+  const objectKeys = Object.keys(object);
+
+  for (const key of objectKeys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "The Wire");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
