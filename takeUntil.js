e02ce0f93a17189349bb93973ffd11b16e6794e6
Charlie C <zbyabqrc@pm.me>
2023-04-10 04:07:15 +0000

    created takeUntil function which takes a slice of an array with elements taken from the beginning until the callback returns a truthy value

---
+++ takeUntil.js
@@ -0,0 +1,54 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    }
+    results.push(item);
+  }
+  return results;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+//eqArrays
+
+const eqArrays = function(arr1, arr2) {
+
+  if (!arr1 || !arr2) {
+    return false;
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  eqArrays(actual, expected) ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//assertion
+
+const data3 = ['coke', 'sprite', 'pepsi', '7-up', 'root beer', 'diet coke', 'orange crush', 'ginger ale'];
+assertArraysEqual(takeUntil(data3, x => x.length >= 8), ['coke', 'sprite', 'pepsi', '7-up']);
+
+const result3 = takeUntil(data3, x => x.length >= 8);
+console.log(result3);
