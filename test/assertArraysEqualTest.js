fa74ce2509243208360da0e422051ae18b815362
Charlie C <zbyabqrc@pm.me>
2023-04-20 22:45:33 +0000

    refactored assertArraysEuqal.js to export all tests to test/assertArraysEqualTest.js

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual');
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]), false); // => false
