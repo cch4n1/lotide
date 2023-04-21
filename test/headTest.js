78088bb4fbe0d4706d3e6a1b7c873dd69e332f10
Charlie C <zbyabqrc@pm.me>
2023-04-21 05:42:49 +0000

    refactored code to use mocha and chai.

--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,40 @@
 const assertEqual = require('../assertEqual');
+// mocha chai 
+const assert = require('chai').assert;
 const head = require('../head');
 
-// assertion tests
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
 
-assertEqual(head([5, 6, 7]), 5);
-//test assertion 1
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-//test assertion 2
-assertEqual(head([3]), 3);
-//An array with one element should yield that one element as its head
-assertEqual(head([]), undefined);
-//An empty array should yield undefined as its head
+describe("#head", () => {
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
+
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+});
+
+describe("#head", () => {
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+describe("#head", () => {
+  it("returns 3 for [3]", () => {
+    assert.strictEqual(head([3]), 3);
+  });
+});
+
+describe("#head", () => {
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([3]), 3);
+  });
+});
