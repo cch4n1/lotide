43b92742e57e87ecdc88137d5c5099b6b9b0c65c
Charlie C <zbyabqrc@pm.me>
2023-04-22 22:27:46 +0000

    refactored to use mocha chai for testing.

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -3,7 +3,30 @@ const countLetters = require("../countLetters");
 
 const result1 = countLetters("Lighthouse Labs");
 
-assertEqual(result1.l, 2)
-assertEqual(result1[" "], undefined)
-assertEqual(result1.c, undefined)
-assertEqual(result1.s, 2)
+// test 1
+describe("#countLetters", () => {
+  it("returns 2", () => {
+    assert.strictEqual(result1.l, 2);
+  });
+});
+
+// test 2
+describe("#countLetters", () => {
+  it("returns undefined", () => {
+    assert.strictEqual(result1[" "], undefined);
+  });
+});
+
+// test 3
+describe("#countLetters", () => {
+  it("returns undefined", () => {
+    assert.strictEqual(result1.c, undefined);
+  });
+});
+
+// test 4
+describe("#countLetters", () => {
+  it("returns 2", () => {
+    assert.strictEqual(result1.s, 2);
+  });
+});
