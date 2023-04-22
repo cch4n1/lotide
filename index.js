dcc3d9da19dbe6f521aa73f198c25df53c72842e
Charlie C <zbyabqrc@pm.me>
2023-04-22 21:35:06 +0000

    created index.js library for all lotide functions.

---
+++ index.js
@@ -0,0 +1,39 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const flatten = require('./flatten');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+
+  head,
+  tail,
+  middle,
+  flatten,
+  countLetters,
+  countOnly,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  eqObjects,
+  assertObjectsEqual,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+
+};
